09ac0d4cc42025fd8ef9c95db84dc097971bc57f
Simon Larsén <slarse@kth.se>
2019-01-25 13:42:31 +0100

    Require newer versions of some packages, fix #8

--- setup.py
+++ setup.py
@@ -1,46 +1,52 @@
 import re
 from setuptools import setup, find_packages
 
-with open('README.md', mode='r', encoding='utf-8') as f:
+with open("README.md", mode="r", encoding="utf-8") as f:
     readme = f.read()
 
 # parse the version instead of importing it to avoid dependency-related crashes
-with open('repomate_junit4/__version.py', mode='r', encoding='utf-8') as f:
+with open("repomate_junit4/__version.py", mode="r", encoding="utf-8") as f:
     line = f.readline()
-    __version__ = line.split('=')[1].strip(" '\"\n")
-    assert re.match(r'^\d\.\d\.\d$', __version__)
+    __version__ = line.split("=")[1].strip(" '\"\n")
+    assert re.match(r"^\d\.\d\.\d$", __version__)
 
 test_requirements = [
-    'appdirs', 'daiquiri', 'pytest', 'pytest-cov>=2.5.1', 'pytest-mock',
-    'codecov'
+    "appdirs",
+    "daiquiri",
+    "pytest>=4.0.0",
+    "pytest-cov>=2.5.1",
+    "pytest-mock",
+    "codecov",
 ]
-required = ['repomate-plug', 'daiquiri', 'colored']
+required = ["repomate-plug>=0.4.0", "daiquiri", "colored"]
 
 setup(
-    name='repomate-junit4',
+    name="repomate-junit4",
     version=__version__,
-    description='JUnit-4.12 plugin for repomate',
+    description="JUnit-4.12 plugin for repomate",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Simon Larsén',
-    author_email='slarse@kth.se',
-    url='https://github.com/slarse/repomate-junit4',
-    download_url='https://github.com/slarse/repomate-junit4/archive/v{}.tar.gz'
-    .format(__version__),
-    license='MIT',
-    packages=find_packages(exclude=('tests', 'docs')),
+    long_description_content_type="text/markdown",
+    author="Simon Larsén",
+    author_email="slarse@kth.se",
+    url="https://github.com/slarse/repomate-junit4",
+    download_url="https://github.com/slarse/repomate-junit4/archive/v{}.tar.gz".format(
+        __version__
+    ),
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
     tests_require=test_requirements,
     install_requires=required,
     extras_require=dict(TEST=test_requirements),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Education',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-    ])
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+    ],
+)
