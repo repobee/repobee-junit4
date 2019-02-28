aae73f1c454e3b6fc11198058b514c50d92e70ee
Simon Larsén <slarse@kth.se>
2019-02-28 17:54:28 +0100

    Add "--skip-existing" to pyenv install

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,9 +4,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     brew upgrade pyenv
     eval "$(pyenv init -)"
-    pyenv install 3.5.4
-    pyenv install 3.6.5
-    pyenv install 3.7.0
+    pyenv install 3.5.4 --skip-existing
+    pyenv install 3.6.5 --skip-existing
+    pyenv install 3.7.0 --skip-existing
     pyenv global 3.7.0
     pip install pip --upgrade
     pip install tox tox-pyenv
