d06b68f1aff7a3819bd32c47c9766c2fef92aab5
Simon Larsén <slarse@kth.se>
2019-03-05 18:13:19 +0100

    Remove setup_hooks fixtures #27

--- tests/test_junit4.py
+++ tests/test_junit4.py
@@ -134,36 +134,27 @@ def getenv_with_classpath(getenv_empty_classpath):
     getenv_empty_classpath.side_effect = side_effect
 
 
-@pytest.fixture
-def setup_hooks():
-    """Fixture that returns a setup function for hooks, with default values for
-    all parameters.
-
-    TODO: Refactor TestActOnClonedRepo to use this instead.
-    """
-
-    def _setup_hooks(
-        reference_tests_dir=RTD,
-        master_repo_names=MASTER_REPO_NAMES,
-        ignore_tests=[],
-        classpath=CLASSPATH,
-        hamcrest_path=HAMCREST_PATH,
-        junit_path=JUNIT_PATH,
-        verbose=False,
-        disable_security=False,
-    ):
-        hooks = junit4.JUnit4Hooks()
-        hooks._reference_tests_dir = reference_tests_dir
-        hooks._master_repo_names = master_repo_names
-        hooks._ignore_tests = ignore_tests
-        hooks._classpath = classpath
-        hooks._hamcrest_path = hamcrest_path
-        hooks._junit_path = junit_path
-        hooks._verbose = verbose
-        hooks._disable_security = disable_security
-        return hooks
-
-    return _setup_hooks
+def setup_hooks(
+    reference_tests_dir=RTD,
+    master_repo_names=MASTER_REPO_NAMES,
+    ignore_tests=[],
+    classpath=CLASSPATH,
+    hamcrest_path=HAMCREST_PATH,
+    junit_path=JUNIT_PATH,
+    verbose=False,
+    disable_security=False,
+):
+    """Return an instance of JUnit4Hooks with pre-configured arguments."""
+    hooks = junit4.JUnit4Hooks()
+    hooks._reference_tests_dir = reference_tests_dir
+    hooks._master_repo_names = master_repo_names
+    hooks._ignore_tests = ignore_tests
+    hooks._classpath = classpath
+    hooks._hamcrest_path = hamcrest_path
+    hooks._junit_path = junit_path
+    hooks._verbose = verbose
+    hooks._disable_security = disable_security
+    return hooks
 
 
 class TestActOnClonedRepo:
@@ -174,56 +165,34 @@ class TestActOnClonedRepo:
         Integration tests, slow running!
     """
 
-    def setup_hooks(
-        self,
-        reference_tests_dir=RTD,
-        master_repo_names=MASTER_REPO_NAMES,
-        ignore_tests=[],
-        classpath=CLASSPATH,
-        hamcrest_path=HAMCREST_PATH,
-        junit_path=JUNIT_PATH,
-        verbose=False,
-    ):
-        hooks = junit4.JUnit4Hooks()
-        hooks._reference_tests_dir = reference_tests_dir
-        hooks._master_repo_names = master_repo_names
-        hooks._ignore_tests = ignore_tests
-        hooks._classpath = classpath
-        hooks._hamcrest_path = hamcrest_path
-        hooks._junit_path = junit_path
-        hooks._verbose = verbose
-        return hooks
-
     @pytest.fixture
-    def hooks(self):
-        return self.setup_hooks()
+    def default_hooks(self):
+        return setup_hooks()
 
-    def test_with_abstract_test_class(self, hooks):
+    def test_with_abstract_test_class(self, default_hooks):
         """Test running the plugin when the reference tests include an abstract test class."""
-        result = hooks.act_on_cloned_repo(ABSTRACT_TEST_REPO)
-
-        print(result.msg)
+        result = default_hooks.act_on_cloned_repo(ABSTRACT_TEST_REPO)
 
         assert result.status == Status.SUCCESS
         assert "Test class PrimeCheckerTest passed!" in result.msg
 
-    def test_correct_repo(self, hooks):
+    def test_correct_repo(self, default_hooks):
         """Test with repo that should not have test failures."""
-        result = hooks.act_on_cloned_repo(SUCCESS_REPO)
+        result = default_hooks.act_on_cloned_repo(SUCCESS_REPO)
 
         assert result.status == Status.SUCCESS
         assert "Test class FiboTest passed!" in result.msg
 
-    def test_fail_repo(self, hooks):
+    def test_fail_repo(self, default_hooks):
         """Test with repo that should have test failures."""
-        result = hooks.act_on_cloned_repo(FAIL_REPO)
+        result = default_hooks.act_on_cloned_repo(FAIL_REPO)
 
         assert result.status == Status.ERROR
         assert "Test class PrimeCheckerTest failed 2 tests" in result.msg
 
     def test_fail_repo_verbose(self):
         """Test verbose output on repo that fails tests."""
-        hooks = self.setup_hooks(verbose=True)
+        hooks = setup_hooks(verbose=True)
 
         expected_verbose_msg = """1) isPrimeFalseForComposites(PrimeCheckerTest)
 java.lang.AssertionError: 
@@ -245,7 +214,7 @@ Expected: is <false>
             dirname = d
         # dir is now deleted
 
-        hooks = self.setup_hooks(reference_tests_dir=dirname)
+        hooks = setup_hooks(reference_tests_dir=dirname)
 
         result = hooks.act_on_cloned_repo(SUCCESS_REPO)
 
@@ -255,52 +224,52 @@ Expected: is <false>
     def test_reference_test_dir_is_file(self):
         """Test with path to reference_tests_dir leading ot a file."""
         with tempfile.NamedTemporaryFile() as file:
-            hooks = self.setup_hooks(reference_tests_dir=file.name)
+            hooks = setup_hooks(reference_tests_dir=file.name)
             result = hooks.act_on_cloned_repo(SUCCESS_REPO)
 
         assert result.status == Status.ERROR
         assert "no reference test directory" in result.msg
 
-    def test_reference_test_dir_has_no_subdir_for_repo(self, hooks):
+    def test_reference_test_dir_has_no_subdir_for_repo(self, default_hooks):
         """Test that a warning is returned when the reference test directory
         has no corresponding subdirectory for the specified repo.
         """
-        result = hooks.act_on_cloned_repo(NO_TEST_DIR_REPO)
+        result = default_hooks.act_on_cloned_repo(NO_TEST_DIR_REPO)
 
         assert result.status == Status.ERROR
         assert "no reference test directory for" in result.msg
 
-    def test_no_tests_for_repo(self, hooks):
+    def test_no_tests_for_repo(self, default_hooks):
         """Test that a warning is returned when the reference test directory
         has a corresponeding subdirectory for the repo, but there are no
         test files in it.
         """
-        result = hooks.act_on_cloned_repo(NO_TESTS_REPO)
+        result = default_hooks.act_on_cloned_repo(NO_TESTS_REPO)
 
         assert result.status == Status.WARNING
         assert "no files ending in `Test.java` found" in result.msg
 
-    def test_error_result_when_no_master_repo_match(self, hooks):
+    def test_error_result_when_no_master_repo_match(self, default_hooks):
         """Test that the result has an error status when the student repo
         has no corresponding master repo (i.e. there is no master repo name
         contained in the student repo name).
         """
-        result = hooks.act_on_cloned_repo(NO_MASTER_MATCH_REPO)
+        result = default_hooks.act_on_cloned_repo(NO_MASTER_MATCH_REPO)
 
         assert result.status == Status.ERROR
         assert "no master repo name matching" in result.msg
 
-    def test_error_result_when_path_does_not_exist(self, hooks):
+    def test_error_result_when_path_does_not_exist(self, default_hooks):
         with tempfile.TemporaryDirectory() as dirname:
             pass
         # dir is now deleted
 
-        result = hooks.act_on_cloned_repo(dirname)
+        result = default_hooks.act_on_cloned_repo(dirname)
 
         assert result.status == Status.ERROR
         assert "student repo {} does not exist".format(dirname) in result.msg
 
-    def test_error_result_when_prod_class_missing(self, hooks):
+    def test_error_result_when_prod_class_missing(self, default_hooks):
         """Test that the result has an error status if the student repo does
         not have a production class for one or more test classes.
         """
@@ -312,43 +281,43 @@ Expected: is <false>
             target = str(pathlib.Path(tmpdir) / "student-week-11")
             shutil.copytree(str(SUCCESS_REPO), target)
 
-            result = hooks.act_on_cloned_repo(target)
+            result = default_hooks.act_on_cloned_repo(target)
 
         assert result.status == Status.ERROR
         assert "no production class found for PrimeCheckerTest" in result.msg
 
-    def test_error_result_on_compile_error(self, hooks):
-        result = hooks.act_on_cloned_repo(str(COMPILE_ERROR_REPO))
+    def test_error_result_on_compile_error(self, default_hooks):
+        result = default_hooks.act_on_cloned_repo(str(COMPILE_ERROR_REPO))
 
         assert result.status == Status.ERROR
         assert "error: illegal start of type" in result.msg
 
-    def test_runs_correctly_when_paths_include_whitespace(self, hooks):
-        result = hooks.act_on_cloned_repo(DIR_PATHS_WITH_SPACES)
+    def test_runs_correctly_when_paths_include_whitespace(self, default_hooks):
+        result = default_hooks.act_on_cloned_repo(DIR_PATHS_WITH_SPACES)
 
         assert result.status == Status.SUCCESS
 
-    def test_runs_with_packaged_code(self, hooks):
+    def test_runs_with_packaged_code(self, default_hooks):
         """Test that packaged code is handled correctly."""
-        result = hooks.act_on_cloned_repo(PACKAGED_CODE_REPO)
+        result = default_hooks.act_on_cloned_repo(PACKAGED_CODE_REPO)
 
         assert result.status == Status.SUCCESS
         assert "Test class se.repomate.fibo.FiboTest passed!" in str(result.msg)
 
-    def test_error_when_student_code_is_incorrectly_packaged(self, hooks):
+    def test_error_when_student_code_is_incorrectly_packaged(self, default_hooks):
         """Test that a test class expecting a package errors out when the
         directory structure in the student repo does not correspond to the
         package statement in the test class.
         """
-        result = hooks.act_on_cloned_repo(NO_DIR_STRUCTURE_REPO)
+        result = default_hooks.act_on_cloned_repo(NO_DIR_STRUCTURE_REPO)
 
         assert result.status == Status.ERROR
 
-    def test_runs_with_multiple_packages(self, hooks):
+    def test_runs_with_multiple_packages(self, default_hooks):
         """Test that a reference test suite with several packages is run
         correctly.
         """
-        result = hooks.act_on_cloned_repo(MULTIPLE_PACKAGES_REPO)
+        result = default_hooks.act_on_cloned_repo(MULTIPLE_PACKAGES_REPO)
 
         assert result.status == Status.SUCCESS
 
@@ -362,7 +331,7 @@ Expected: is <false>
             _CP.format("garbage/path", HAMCREST_PATH, ".", JUNIT_PATH),
         ],
     )
-    def test_jars_found_on_classpath(self, setup_hooks, classpath):
+    def test_jars_found_on_classpath(self, classpath):
         """Test that acting on a repo when the hamcrest and junit jars are only
         specified on the classpath works as intended.
         """
@@ -378,7 +347,7 @@ class TestSecurityPolicy:
     to unauthorized resources.
     """
 
-    def test_error_on_unauthorized_read(self, setup_hooks):
+    def test_error_on_unauthorized_read(self):
         """Test that the default security policy blocks read access to
         files.
         """
@@ -389,7 +358,7 @@ class TestSecurityPolicy:
         assert result.status == Status.ERROR
         assert "java.security.AccessControlException: access denied" in result.msg
 
-    def test_error_on_unauthorized_network_access(self, setup_hooks):
+    def test_error_on_unauthorized_network_access(self):
         """Test that the default security policy blocks network access."""
         hooks = setup_hooks(verbose=True)
 
@@ -398,7 +367,7 @@ class TestSecurityPolicy:
         assert result.status == Status.ERROR
         assert "java.security.AccessControlException: access denied" in result.msg
 
-    def test_file_access_allowed_with_disabled_security(self, setup_hooks):
+    def test_file_access_allowed_with_disabled_security(self):
         """Test that student code can access files without crashing if security
         is disabled.
         """
