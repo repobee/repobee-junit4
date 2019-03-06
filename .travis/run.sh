e0b03b8369e43fbb9e42754db6f09a5283927044
Simon Larsén <slarse@kth.se>
2019-03-06 18:41:39 +0100

    Add flake8 to ci, fix #36

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+function run_flake8() {
+    pip install flake8
+    flake8 .
+}
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     eval "$(pyenv init -)"
     pyenv local 3.5.4 3.6.5 3.7.0
     pyenv global 3.7.0
+    run_flake8
     tox
 else
+    run_flake8
     pytest tests --cov=repomate_junit4
 fi
