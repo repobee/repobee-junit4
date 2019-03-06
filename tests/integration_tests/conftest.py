688168c5dc9478c4836f0fc0c2130ce66d785890
Simon Larsén <slarse@kth.se>
2019-03-06 10:52:04 +0100

    Add fixtures that clean up Java .class files

---
+++ tests/integration_tests/conftest.py
@@ -0,0 +1,26 @@
+"""Fixtures for the integrations tests."""
+import pytest
+from pathlib import Path
+
+
+def _remove_class_files(root=Path(__file__).parent):
+    """Finds and removes every .class file in this directory, or any
+    subdirectory.
+    """
+    class_files = list(root.rglob("*.class"))
+    for file in class_files:
+        file.unlink()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def remove_class_files_session():
+    """Remove class files before and after the test session starts."""
+    _remove_class_files()
+    yield
+    _remove_class_files()
+
+
+@pytest.fixture(autouse=True)
+def remove_class_files_after_test_function():
+    """Remove class files before each test executes."""
+    _remove_class_files()
