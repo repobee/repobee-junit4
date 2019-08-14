bb81b8899a7ee2eac206583a26893733810aa5d1
Simon Larsén <slarse@kth.se>
2019-08-14 10:56:34 +0200

    Bump patch version number to 0.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Simon Larsén"
 # The short X.Y version
 version = "0.6"
 # The full version, including alpha/beta/rc tags
-release = "0.6.0"
+release = "0.6.1"
 
 
 # -- General configuration ---------------------------------------------------
