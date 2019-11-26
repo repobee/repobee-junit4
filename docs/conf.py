52527a24de0ce30b27629ae1004b135bc25ba96d
Simon Larsén <slarse@kth.se>
2019-11-26 14:37:16 +0100

    Bump minor version number to 0.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, Simon Larsén"
 author = "Simon Larsén"
 
 # The short X.Y version
-version = "0.6"
+version = "0.7"
 # The full version, including alpha/beta/rc tags
-release = "0.6.1"
+release = "0.7.0"
 
 
 # -- General configuration ---------------------------------------------------
