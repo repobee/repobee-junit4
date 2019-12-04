66cd1613d9a692b995a63428034b6a717ed797dc
Simon Larsén <slarse@kth.se>
2019-12-04 18:50:45 +0100

    Add missing test repo

---
+++ tests/integration_tests/repos/endless-loop-week-10/src/Fibo.java
@@ -0,0 +1,25 @@
+/**
+ * Class for calculating Fibonacci numbers.
+ */
+
+public class Fibo {
+    private long prev;
+    private long current;
+
+    public Fibo() {
+        prev = 0;
+        current = 1;
+    }
+
+    /**
+     * Generate the next Fibonacci number.
+     */
+    public long next() {
+        long ret = prev;
+        prev = current;
+        current = ret + current;
+        while (current < current + 1)
+            ;
+        return ret;
+    }
+}
