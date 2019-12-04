98c6b9d9c6ea0a4bf210e17b9a5368201227d45a
Simon Larsén <slarse@kth.se>
2019-12-04 19:01:34 +0100

    [feat] Add `--junit4-timeout` option to specify a hard time limit for tests (#69)

--- repobee_junit4/_output.py
+++ repobee_junit4/_output.py
@@ -9,10 +9,12 @@ import sys
 import os
 import re
 import collections
+import pathlib
+import subprocess
+import math
+from typing import Optional
 
 from colored import bg, style
-
-from repobee_plug import Status
 
 from repobee_junit4 import _java
 
@@ -24,83 +26,122 @@ DEFAULT_MAX_LINES = 10
 
 
 class TestResult(
-    collections.namedtuple("TestResult", "test_class proc".split())
+    collections.namedtuple(
+        "TestResult",
+        "fqn success num_failed num_passed test_failures timeout".split(),
+    )
 ):
-    @property
-    def fqn(self):
-        return _java.fqn_from_file(self.test_class)
+    """An immutable class for storing test results. Outside callers should use
+    the static build methods :py:meth:`TestResult.build` or
+    :py:meth:`TestResult.timed_out` to create instances.
 
-    @property
-    def success(self):
-        return self.proc.returncode == 0
+    Attributes:
+        fqn (str): The fully qualified name of the test class.
+        success (bool): True if the test exited with a 0 exit status.
+        num_failed (int): The amount of test cases that failed.
+        num_passed (int): The amount of test cases that passed.
+        test_failures (List[str]): A list of test failure messages.
+        timeout (Optional[int]): The amount of seconds after which the test
+            class timed out, or None if it did not time out.
+    """
 
-    @property
-    def status(self):
-        return Status.SUCCESS if self.success else Status.ERROR
+    @staticmethod
+    def build(
+        test_class: pathlib.Path, proc: subprocess.CompletedProcess
+    ) -> "TestResult":
+        """Build a TestResult.
 
-    @property
-    def num_failed(self):
-        return _get_num_failed(self.proc.stdout)
+        Args:
+            test_class: Path to the test class.
+            proc: A completed process of running the test class.
+        Returns:
+            A TestResult instance representing the test run.
+        """
+        stdout = proc.stdout.decode(encoding=sys.getdefaultencoding())
+        return TestResult(
+            fqn=_java.fqn_from_file(test_class),
+            success=proc.returncode == 0,
+            num_failed=_get_num_failed(stdout),
+            num_passed=_get_num_passed(stdout),
+            test_failures=_parse_failed_tests(stdout),
+            timeout=None,
+        )
 
-    @property
-    def num_passed(self):
-        return _get_num_passed(self.proc.stdout)
+    @staticmethod
+    def timed_out(test_class: pathlib.Path, timeout: int):
+        """Create a TestResult instance from a test that timed out.
 
-    @property
-    def test_failures(self):
-        return _parse_failed_tests(self.proc.stdout)
+        Args:
+            test_class: Path to the test class.
+            timeout: Amount of seconds after which the test class timed out.
+        Returns:
+            A TestResult instance representing the timed out test run.
+        """
+        return TestResult(
+            fqn=_java.fqn_from_file(test_class),
+            success=False,
+            num_failed=0,
+            num_passed=0,
+            test_failures=list(),
+            timeout=timeout,
+        )
 
     def pretty_result(self, verbose: bool) -> str:
         """Format this test as a pretty-printed message."""
         title_color = SUCCESS_COLOR if self.success else FAILURE_COLOR
-        num_passed = self.num_passed
-        num_failed = self.num_failed
         msg = test_result_header(
-            self.fqn, num_passed + num_failed, num_passed, title_color
+            self.fqn,
+            self.num_passed + self.num_failed,
+            self.num_passed,
+            title_color,
+            self.timeout,
         )
         if not self.success and verbose:
             msg += os.linesep + os.linesep.join(self.test_failures)
         return msg
 
 
-def _get_num_failed(test_output: bytes) -> int:
+def _get_num_failed(test_output: str) -> int:
     """Get the amount of failed tests from the error output of JUnit4."""
-    decoded = test_output.decode(encoding=sys.getdefaultencoding())
-    match = re.search(r"Failures: (\d+)", decoded)
+    match = re.search(r"Failures: (\d+)", test_output)
     return int(match.group(1)) if match else 0
 
 
-def _get_num_tests(test_output: bytes) -> int:
+def _get_num_tests(test_output: str) -> int:
     """Get the total amount of tests. Only use this if there were test
     failures!
     """
-    decoded = test_output.decode(encoding=sys.getdefaultencoding())
-    match = re.search(r"Tests run: (\d+)", decoded)
+    match = re.search(r"Tests run: (\d+)", test_output)
     return int(match.group(1)) if match else 0
 
 
-def _get_num_passed(test_output: bytes) -> int:
+def _get_num_passed(test_output: str) -> int:
     """Get the amount of passed tests from the output of JUnit4."""
-    decoded = test_output.decode(encoding=sys.getdefaultencoding())
-    match = re.search(r"OK \((\d+) tests\)", decoded)
+    match = re.search(r"OK \((\d+) tests\)", test_output)
     if not match:  # there were failures
         return _get_num_tests(test_output) - _get_num_failed(test_output)
     return int(match.group(1))
 
 
-def _parse_failed_tests(test_output: bytes) -> str:
+def _parse_failed_tests(test_output: str) -> str:
     """Return a list of test failure descriptions, excluding stack traces."""
-    decoded = test_output.decode(encoding=sys.getdefaultencoding())
     return re.findall(
-        r"^\d\) .*(?:\n(?!\s+at).*)*", decoded, flags=re.MULTILINE
+        r"^\d\) .*(?:\n(?!\s+at).*)*", test_output, flags=re.MULTILINE
     )
 
 
 def test_result_header(
-    test_class_name: str, num_tests: int, num_passed: int, title_color: bg
+    test_class_name: str,
+    num_tests: int,
+    num_passed: int,
+    title_color: bg,
+    timeout: Optional[int] = None,
 ) -> str:
     """Return the header line for a test result."""
-    test_results = "Passed {}/{} tests".format(num_passed, num_tests)
+    if timeout is None:
+        test_results = "Passed {}/{} tests".format(num_passed, num_tests)
+    else:
+        test_results = "Timed out after {} seconds".format(math.ceil(timeout))
     msg = "{}{}{}: {}".format(
         title_color, test_class_name, style.RESET, test_results
     )
