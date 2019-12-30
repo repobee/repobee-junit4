14797b7eedceaa721a089d576007237812adaa26
Simon Larsén <slarse@kth.se>
2019-12-30 12:56:41 +0100

    Follow redirects for downloading jars

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,5 +11,5 @@ fi
 java -version
 pip install -e ".[TEST]"
 
-curl https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -o junit-4.12.jar
-curl https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -o hamcrest-core-1.3.jar
+curl -L https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -o junit-4.12.jar
+curl -L https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -o hamcrest-core-1.3.jar
