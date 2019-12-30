1562b526d6a2cf833cbb1c0bc1ed4c01e4396258
Simon Larsén <slarse@kth.se>
2019-12-30 12:59:06 +0100

    [misc] Follow redirects when downloading jars for integration tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,5 +10,5 @@ fi
 
 pip install -e ".[TEST]"
 
-curl https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -o junit-4.12.jar
-curl https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -o hamcrest-core-1.3.jar
+curl -L https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -o junit-4.12.jar
+curl -L https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -o hamcrest-core-1.3.jar
