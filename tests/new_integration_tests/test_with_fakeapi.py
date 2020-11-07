0fe5fcefb94c81cfdd23e58bc5d1aa6a4a75eaa0
Simon Larsén <slarse@slar.se>
2020-11-07 16:49:11 +0100

    Strengthen tests

--- tests/new_integration_tests/test_with_fakeapi.py
+++ tests/new_integration_tests/test_with_fakeapi.py
@@ -8,8 +8,10 @@ import pytest
 import git
 
 import repobee_testhelpers
+import repobee_plug as plug
 
 from repobee_junit4 import junit4
+from repobee_junit4 import SECTION
 
 
 class TestGenerateRTD:
@@ -18,24 +20,19 @@ class TestGenerateRTD:
     def test_generate_reference_tests_directory(
         self, tmp_path_factory, platform_url
     ):
+        # arrange
         workdir = tmp_path_factory.mktemp("workdir")
         rtd_path = workdir / "test-reference-tests-directory"
 
-        repobee_testhelpers.funcs.run_repobee(
-            f"junit4 generate-rtd -a {ASSIGNMENTS_ARG} "
-            f"--base-url {platform_url} "
-            f"--template-org-name "
-            f"{repobee_testhelpers.const.TEMPLATE_ORG_NAME} "
-            f"--branch {SOLUTIONS_BRANCH} "
-            f"--reference-tests-dir {rtd_path}",
-            plugins=[junit4],
-            workdir=workdir,
-        )
+        # act
+        run_generate_rtd(base_url=platform_url, rtd=rtd_path, workdir=workdir)
 
+        # assert
         rtd_subdirs = list(rtd_path.iterdir())
-
         assert set(ASSIGNMENT_NAMES) == {subdir.name for subdir in rtd_subdirs}
+        iterations = 0
         for assignment_name in ASSIGNMENT_NAMES:
+            iterations += 1
             assignment_tests_dir = rtd_path / assignment_name
             test_files = {
                 f.relative_to(assignment_tests_dir)
@@ -43,32 +40,42 @@ class TestGenerateRTD:
             }
             assert test_files == EXPECTED_REFERENCE_TESTS[assignment_name]
 
+        assert iterations > 0, "the assertion loop did not execute"
+
     def test_use_generated_reference_tests_directory(
         self, tmp_path_factory, platform_url, setup_student_repos
     ):
+        """Test using a generated RTD with the clone command."""
+        # arrange
         workdir = tmp_path_factory.mktemp("workdir")
         rtd_path = workdir / "test-reference-tests-directory"
+        run_generate_rtd(base_url=platform_url, rtd=rtd_path, workdir=workdir)
+        clone_dir = workdir / "clone_dir"
+        clone_dir.mkdir()
 
-        repobee_testhelpers.funcs.run_repobee(
-            f"junit4 generate-rtd -a {ASSIGNMENTS_ARG} "
-            f"--base-url {platform_url} "
-            f"--template-org-name "
-            f"{repobee_testhelpers.const.TEMPLATE_ORG_NAME} "
-            f"--branch {SOLUTIONS_BRANCH} "
-            f"--reference-tests-dir {rtd_path}",
-            plugins=[junit4],
-            workdir=workdir,
-        )
-
-        repobee_testhelpers.funcs.run_repobee(
+        # act
+        results = repobee_testhelpers.funcs.run_repobee(
             f"repos clone -a {ASSIGNMENTS_ARG} "
             f"--base-url {platform_url} "
             f"--junit4-reference-tests-dir {rtd_path} "
             f"--junit4-hamcrest-path {HAMCREST_PATH} "
             f"--junit4-junit-path {JUNIT_PATH} ",
             plugins=[junit4],
-            workdir=workdir,
+            workdir=clone_dir,
         )
+
+        # assert
+        iterations = 0
+        for repo_name in plug.generate_repo_names(
+            repobee_testhelpers.const.STUDENT_TEAMS, ASSIGNMENT_NAMES
+        ):
+            iterations += 1
+            first_result, *rest = results[repo_name]
+            assert not rest, "there should only be one result"
+            assert first_result.name == SECTION
+            assert first_result.status != plug.Status.ERROR
+
+        assert iterations > 0, "the assertion loop did not execute"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -114,6 +121,27 @@ HAMCREST_PATH = pathlib.Path(os.getenv("REPOBEE_JUNIT4_JUNIT")).resolve(
 JUNIT_PATH = pathlib.Path(os.getenv("REPOBEE_JUNIT4_HAMCREST")).resolve(
     strict=True
 )
+
+
+def run_generate_rtd(
+    base_url: str,
+    rtd: pathlib.Path,
+    workdir: pathlib.Path,
+    template_org_name: str = repobee_testhelpers.const.TEMPLATE_ORG_NAME,
+    branch: str = SOLUTIONS_BRANCH,
+    assignments: str = ASSIGNMENTS_ARG,
+):
+    """Helper for running the generate-rtd command."""
+    return repobee_testhelpers.funcs.run_repobee(
+        f"junit4 generate-rtd -a {assignments} "
+        f"--base-url {base_url} "
+        f"--template-org-name "
+        f"{repobee_testhelpers.const.TEMPLATE_ORG_NAME} "
+        f"--branch {SOLUTIONS_BRANCH} "
+        f"--reference-tests-dir {rtd}",
+        plugins=[junit4],
+        workdir=workdir,
+    )
 
 
 @pytest.fixture(autouse=True)
