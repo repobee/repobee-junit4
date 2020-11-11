dd70a7c4f8ec446feee8438dc22ec12fcbee06c6
Simon Larsén <slarse@slar.se>
2020-11-11 19:15:52 +0100

    [test] Use GitHub Actions for running tests (#91)

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-function run_flake8() {
-    pip install flake8
-    flake8 .
-}
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    eval "$(pyenv init -)"
-    pyenv global 3.6.10
-fi
-
-run_flake8
 pytest tests --cov=repobee_junit4 --cov-branch
