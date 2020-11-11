100694477df358048f9c389b5a51777da609d1f7
Simon Larsén <slarse@slar.se>
2020-11-11 19:27:48 +0100

    Use junit-4.12

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install -e ".[TEST]"
 
-curl -L https://search.maven.org/remotecontent?filepath=junit/junit/4.13.1/junit-4.13.1.jar -o junit-4.13.1.jar
+curl -L https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar -o junit-4.12.jar
 curl -L https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -o hamcrest-core-1.3.jar
