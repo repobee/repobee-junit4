d4ca36f3ac406177eb8c3abc25641e20d9d1b308
Simon Larsén <slarse@slar.se>
2022-09-25 17:54:30 +0200

    Reproduce error

---
+++ tests/integration_tests/repos/directory-with-java-suffix-week-10/src/Fibo.java/Fibo.java
@@ -0,0 +1,23 @@
+/**
+ * Class for calculating Fibonacci numbers.
+ */
+
+public class Fibo {
+    private long prev;
+    private long current;
+
+    public Fibo() {
+        prev = 0;
+        current = 1;
+    }
+
+    /**
+     * Generate the next Fibonacci number.
+     */
+    public long next() {
+        long ret = prev;
+        prev = current;
+        current = ret + current;
+        return ret;
+    }
+}
